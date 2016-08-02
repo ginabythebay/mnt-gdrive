0c994a0128fd22873686615a6f250b6d369dc6e1
Gina White <ginabythebay@gmail.com>
2016-08-01 20:46:04 -0700

    extract out fakedrive stuff
    so it can be shared by other tests

--- integration_test.go
+++ integration_test.go
@@ -4,131 +4,29 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
-	"math/rand"
 	"os"
 	"path"
 	"testing"
 
+	"github.com/ginabythebay/mnt-gdrive/internal/fakedrive"
 	"github.com/ginabythebay/mnt-gdrive/internal/gdrive"
 
-	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"bazil.org/fuse/fs/fstestutil"
-	"golang.org/x/net/context"
 )
 
 func init() {
 	fstestutil.DebugByDefault()
 }
 
-func pseudo_uuid() (uuid string) {
-
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-
-	uuid = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-
-	return
-}
-
 func allNodes() []*gdrive.Node {
 	return []*gdrive.Node{
-		makeDir("root", "", ""),
-		makeDir("dir_one_id", "dir one", "root"),
-		makeDir("dir_two_id", "dir two", "root"),
-		makeTextFile("file_one_id", "file one", "root"),
-		makeTextFile("file_two_id", "file two", "dir_two_id"),
+		fakedrive.MakeDir("root", "", ""),
+		fakedrive.MakeDir("dir_one_id", "dir one", "root"),
+		fakedrive.MakeDir("dir_two_id", "dir two", "root"),
+		fakedrive.MakeTextFile("file_one_id", "file one", "root"),
+		fakedrive.MakeTextFile("file_two_id", "file two", "dir_two_id"),
 	}
-}
-
-func makeDir(id string, name string, parentID string) *gdrive.Node {
-	parents := []string{}
-	if parentID != "" {
-		parents = []string{parentID}
-	}
-	return &gdrive.Node{ID: id, Name: name, ParentIDs: parents, MimeType: "application/vnd.google-apps.folder"}
-}
-
-func contentForTextFile(id string) []byte {
-	return []byte(fmt.Sprintf("content for %s", id))
-}
-
-func makeTextFile(id string, name string, parentId string) *gdrive.Node {
-	parents := []string{parentId}
-	n := &gdrive.Node{
-		ID:            id,
-		Name:          name,
-		ParentIDs:     parents,
-		MimeType:      "text/plain",
-		FileExtension: ".txt"}
-	n.Size = uint64(len(contentForTextFile(id)))
-	return n
-}
-
-type fakeDrive struct {
-	allNodes []*gdrive.Node
-}
-
-func (fake *fakeDrive) newId() (id string) {
-	idSet := map[string]bool{}
-	for _, n := range fake.allNodes {
-		idSet[n.ID] = true
-	}
-	for {
-		candidate := pseudo_uuid()
-		if _, found := idSet[candidate]; !found {
-			return candidate
-		}
-	}
-}
-
-func (fake *fakeDrive) FetchNode(id string) (n *gdrive.Node, err error) {
-	for _, n := range fake.allNodes {
-		if n.ID == id {
-			return n, nil
-		}
-	}
-	return nil, fuse.ENOENT
-}
-
-func (fake *fakeDrive) CreateNode(parentID string, name string, dir bool) (n *gdrive.Node, err error) {
-	id := fake.newId()
-	if dir {
-		n = makeDir(id, name, parentID)
-	} else {
-		n = makeTextFile(id, name, parentID)
-	}
-	return n, nil
-}
-
-func (fake *fakeDrive) FetchChildren(ctx context.Context, id string) (children []*gdrive.Node, err error) {
-	if _, err := fake.FetchNode(id); err != nil {
-		return nil, err
-	}
-	for _, n := range fake.allNodes {
-		for _, p := range n.ParentIDs {
-			if p == id {
-				children = append(children, n)
-			}
-		}
-	}
-
-	return children, nil
-}
-
-func (fake *fakeDrive) Download(id string, f *os.File) error {
-	f.Write(contentForTextFile(id))
-	return nil
-}
-
-func (fake *fakeDrive) ProcessChanges(changeHandler func(*gdrive.Change, *gdrive.ChangeStats)) (gdrive.ChangeStats, error) {
-	log.Fatal("implement me")
-	return gdrive.ChangeStats{}, fuse.EIO
 }
 
 func neverErr(fi os.FileInfo) error {
@@ -139,7 +37,7 @@ func TestScenario(t *testing.T) {
 	fmt.Print("before mount func thing\n")
 	var sys *system
 	mntFunc := func(mnt *fstestutil.Mount) fs.FS {
-		sys = newSystem(&fakeDrive{allNodes()}, mnt.Server, true)
+		sys = newSystem(fakedrive.NewDrive(allNodes()), mnt.Server, true)
 		return sys
 	}
 	mnt, err := fstestutil.MountedFuncT(t, mntFunc, nil)
@@ -173,7 +71,7 @@ func TestScenario(t *testing.T) {
 	createFileThreeChange := gdrive.Change{
 		ID:      "file_three_id",
 		Removed: false,
-		Node:    makeTextFile("file_three_id", "file three", "dir_two_id"),
+		Node:    fakedrive.MakeTextFile("file_three_id", "file three", "dir_two_id"),
 	}
 	cs := gdrive.ChangeStats{}
 	verifyChangeStats(t, "init", gdrive.ChangeStats{}, cs)
---
+++ internal/fakedrive/fakedrive.go
@@ -0,0 +1,124 @@
+package fakedrive
+
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"os"
+
+	"bazil.org/fuse"
+	"golang.org/x/net/context"
+
+	"github.com/ginabythebay/mnt-gdrive/internal/gdrive"
+)
+
+func pseudoUUID() (uuid string) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	uuid = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+
+	return
+}
+
+// MakeDir returns a new gdrive directory, suitable for testing.
+func MakeDir(id string, name string, parentID string) *gdrive.Node {
+	parents := []string{}
+	if parentID != "" {
+		parents = []string{parentID}
+	}
+	return &gdrive.Node{ID: id, Name: name, ParentIDs: parents, MimeType: "application/vnd.google-apps.folder"}
+}
+
+func contentForTextFile(id string) []byte {
+	return []byte(fmt.Sprintf("content for %s", id))
+}
+
+// MakeTextFile returns a new gdrive text file, suitable for testing.
+func MakeTextFile(id string, name string, parentID string) *gdrive.Node {
+	parents := []string{parentID}
+	n := &gdrive.Node{
+		ID:            id,
+		Name:          name,
+		ParentIDs:     parents,
+		MimeType:      "text/plain",
+		FileExtension: ".txt"}
+	n.Size = uint64(len(contentForTextFile(id)))
+	return n
+}
+
+// Drive represents a fake drive, for integration testing
+type Drive struct {
+	allNodes []*gdrive.Node
+}
+
+// NewDrive returns a new fake drive.
+func NewDrive(allNodes []*gdrive.Node) *Drive {
+	return &Drive{allNodes}
+}
+
+func (fake *Drive) newID() (id string) {
+	idSet := map[string]bool{}
+	for _, n := range fake.allNodes {
+		idSet[n.ID] = true
+	}
+	for {
+		candidate := pseudoUUID()
+		if _, found := idSet[candidate]; !found {
+			return candidate
+		}
+	}
+}
+
+// FetchNode looks up a node by id in our in-memory data structure.
+func (fake *Drive) FetchNode(id string) (n *gdrive.Node, err error) {
+	for _, n := range fake.allNodes {
+		if n.ID == id {
+			return n, nil
+		}
+	}
+	return nil, fuse.ENOENT
+}
+
+// CreateNode creates a fake node and puts it into our in memory data structure.
+func (fake *Drive) CreateNode(parentID string, name string, dir bool) (n *gdrive.Node, err error) {
+	id := fake.newID()
+	if dir {
+		n = MakeDir(id, name, parentID)
+	} else {
+		n = MakeTextFile(id, name, parentID)
+	}
+	return n, nil
+}
+
+// FetchChildren looks up the children in memory for an id.
+func (fake *Drive) FetchChildren(ctx context.Context, id string) (children []*gdrive.Node, err error) {
+	if _, err := fake.FetchNode(id); err != nil {
+		return nil, err
+	}
+	for _, n := range fake.allNodes {
+		for _, p := range n.ParentIDs {
+			if p == id {
+				children = append(children, n)
+			}
+		}
+	}
+
+	return children, nil
+}
+
+// Download copies content for our in memory node into a file.
+func (fake *Drive) Download(id string, f *os.File) error {
+	f.Write(contentForTextFile(id))
+	return nil
+}
+
+// ProcessChanges doesn't work yet.
+func (fake *Drive) ProcessChanges(changeHandler func(*gdrive.Change, *gdrive.ChangeStats)) (gdrive.ChangeStats, error) {
+	log.Fatal("implement me")
+	return gdrive.ChangeStats{}, fuse.EIO
+}
